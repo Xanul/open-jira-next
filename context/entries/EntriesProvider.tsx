c5d7e39e66179b237222831024490d61e74ab0af
Xanul <rivas.060@gmail.com>
2023-01-23 19:41:11 -0600

    Configured the entries context and added the Entry interface

---
+++ context/entries/EntriesProvider.tsx
@@ -0,0 +1,48 @@
+import React, { FC, PropsWithChildren, useReducer } from 'react';
+import { Entry } from '../../interfaces';
+import { EntriesContext, entriesReducer } from './';
+import { v4 as uuidv4 } from 'uuid';
+
+
+
+export interface EntriesState {
+  entries: Entry[];
+}
+
+const ENTRIES_INITIAL_STATE: EntriesState = {
+  entries: [
+    {
+      _id: uuidv4(),
+      description: 'lorem ipsum prodient dolo duis et dolor laborum veniam ea laboris qui consequat',
+      status: 'pending',
+      createdAt: Date.now()
+    },
+    {
+      _id: uuidv4(),
+      description: 'lorem ipsum prodient dolo duis et dolor laborum veniam ea laboris qui consequat',
+      status: 'in-progress',
+      createdAt: Date.now() - 1000000,
+    },
+    {
+      _id: uuidv4(),
+      description: 'lorem ipsum prodient dolo duis et dolor laborum veniam ea laboris qui consequat',
+      status: 'finished',
+      createdAt: Date.now() - 200000,
+    }
+  ],
+}
+
+export const EntriesProvider:FC<PropsWithChildren> = ({ children }) => {
+
+  const [state, dispatch] = useReducer(entriesReducer, ENTRIES_INITIAL_STATE);
+
+
+
+  return (
+    <EntriesContext.Provider value={{
+      ...state,
+    }}>
+      { children }
+    </EntriesContext.Provider>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,21 @@ import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { lightTheme, darkTheme } from '../themes'; // Imported themes from ./themes
 import { UiProvider } from '../context/ui';
+import { EntriesProvider } from '../context/entries';
 
 
 
 // We add Theme provider and Cssbaseline to apply the styles to the whole project
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UiProvider>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </UiProvider>
-    
+    <EntriesProvider>
+      <UiProvider>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UiProvider>  
+    </EntriesProvider>
   )
 }
 
